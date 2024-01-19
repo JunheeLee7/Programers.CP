edeb74c05ad61513594be9d6c9fec910ba2b0c4c
JunheeLee7 <120552023+JunheeLee7@users.noreply.github.com>
2024-01-20 00:53:48 +0900

    Create [프로그래머스 C++] 순서 바꾸기.cpp

---
+++ [프로그래머스 C++] 순서 바꾸기.cpp
@@ -0,0 +1,25 @@
+◈ 문제 설명 링크
+
+https://school.programmers.co.kr/learn/courses/30/lessons/181891
+
+◈ 작성 코드
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) 
+{
+    vector<int> answer;
+    for(int i = n; i < num_list.size(); i++)
+    {
+        answer.push_back(num_list[i]);
+    }
+    
+    for(int j = 0; j < n; j++)
+    {
+        answer.push_back(num_list[j]);
+    }
+    return answer;
+}
