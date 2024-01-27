0be0d295433b0d462970520398823e806236f13e
JunheeLee7 <120552023+JunheeLee7@users.noreply.github.com>
2024-01-27 23:20:47 +0900

    Create [프로그래머스 C++] rny_string.cpp

---
+++ [프로그래머스 C++] rny_string.cpp
@@ -0,0 +1,28 @@
+◈ 문제 설명 링크
+
+
+
+◈ 작성 코드
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string rny_string) 
+{
+    string answer = "";
+    for(int i = 0; i < rny_string.size(); i++)
+    {
+        if(rny_string[i] == 'm')
+        {
+            answer.push_back('r');
+            answer.push_back('n');
+        }
+        else
+        {
+            answer.push_back(rny_string[i]);
+        }
+    }
+    return answer;
+}
