fdfd6f62457e83d0c29772a43db7e31cf46973f1
JunheeLee7 <120552023+JunheeLee7@users.noreply.github.com>
2024-01-28 23:36:46 +0900

    Create [프로그래머스 C++] 뒤에서 5등까지.cpp

---
+++ [프로그래머스 C++] 뒤에서 5등까지.cpp
@@ -0,0 +1,25 @@
+◈ 문제 설명 링크
+
+https://school.programmers.co.kr/learn/courses/30/lessons/181853
+
+◈ 작성 코드
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) 
+{
+    vector<int> answer;
+    sort(num_list.begin(), num_list.end());
+    for(int i = 0; i < 5; i++)
+    {
+        answer.push_back(num_list[i]);
+    }
+    
+    return answer;
+}
+
+
