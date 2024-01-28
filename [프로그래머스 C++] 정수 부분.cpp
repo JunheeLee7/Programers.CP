4d3a234dbcd74581806046f6663f1230c38a6034
JunheeLee7 <120552023+JunheeLee7@users.noreply.github.com>
2024-01-28 23:39:41 +0900

    Create [프로그래머스 C++] 정수 부분.cpp

---
+++ [프로그래머스 C++] 정수 부분.cpp
@@ -0,0 +1,17 @@
+◈ 문제 설명 링크
+
+https://school.programmers.co.kr/learn/courses/30/lessons/181850
+
+◈ 작성 코드
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(double flo) 
+{
+    int answer = flo;
+    return answer;
+}
+
