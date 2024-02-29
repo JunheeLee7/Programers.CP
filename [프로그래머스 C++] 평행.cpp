b3c7efad8ae0528ba04d3f5615b28395209fd04f
JunheeLee7 <120552023+JunheeLee7@users.noreply.github.com>
2024-02-29 23:17:42 +0900

    Create [프로그래머스 C++] 평행.cpp

---
+++ [프로그래머스 C++] 평행.cpp
@@ -0,0 +1,46 @@
+◈ 문제 설명 링크
+
+https://school.programmers.co.kr/learn/courses/30/lessons/120875
+
+◈ 작성 코드
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> dots) 
+{
+    int answer = 0;
+    double a=dots[0][0] - dots[1][0];
+    double b=dots[0][1] - dots[1][1];
+    double c=dots[2][0] - dots[3][0];
+    double d=dots[2][1] - dots[3][1];
+    
+    if(a/b == c/d) 
+    {
+        return 1;
+    }
+    
+    a=dots[0][0] - dots[2][0];
+    b=dots[0][1] - dots[2][1];
+    c=dots[1][0] - dots[3][0];
+    d=dots[1][1] - dots[3][1];
+    
+    if(a/b == c/d) 
+    {
+        return 1;
+    }
+    
+    a=dots[0][0] - dots[3][0];
+    b=dots[0][1] - dots[3][1];
+    c=dots[1][0] - dots[2][0];
+    d=dots[1][1] - dots[2][1];
+    
+    if(a/b == c/d) 
+    {
+        return 1;
+    }
+    
+    return answer;
+}
