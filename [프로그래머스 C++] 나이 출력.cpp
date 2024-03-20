605bab7850d130e0b1a28d7f3dc44faea227aad6
JunheeLee7 <120552023+JunheeLee7@users.noreply.github.com>
2024-03-20 23:50:08 +0900

    Create [프로그래머스 C++] 나이 출력.cpp

---
+++ [프로그래머스 C++] 나이 출력.cpp
@@ -0,0 +1,17 @@
+◈ 문제 설명 링크
+
+https://school.programmers.co.kr/learn/courses/30/lessons/120820
+
+◈ 작성 코드
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int age) 
+{
+    int answer = 2023 - age;
+    return answer;
+}
+
