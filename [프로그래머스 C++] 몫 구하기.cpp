af6fd3b8c3546cb5cf947445e1745cd0b0119e7b
JunheeLee7 <120552023+JunheeLee7@users.noreply.github.com>
2024-04-17 22:41:57 +0900

    Create [프로그래머스 C++] 몫 구하기.cpp

---
+++ [프로그래머스 C++] 몫 구하기.cpp
@@ -0,0 +1,17 @@
+◈ 문제 설명 링크
+
+https://school.programmers.co.kr/learn/courses/30/lessons/120805
+
+◈ 작성 코드
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) 
+{
+    int answer = 0;
+    
+    return answer = num1 / num2;
+}
