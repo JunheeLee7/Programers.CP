d0912cc327737c2f681c6071c1b18e24f0dc9290
JunheeLee7 <120552023+JunheeLee7@users.noreply.github.com>
2024-04-17 22:42:20 +0900

    Create [프로그래머스 C++] 두 수의 곱.cpp

---
+++ [프로그래머스 C++] 두 수의 곱.cpp
@@ -0,0 +1,18 @@
+◈ 문제 설명 링크
+
+https://school.programmers.co.kr/learn/courses/30/lessons/120804
+
+◈ 작성 코드
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) 
+{
+    int answer = num1 * num2;
+    
+    return answer;
+}
+
